0f3472e8c1331739fbfb89f53b9b701f17f6e259
yovanny <yovanny@LAPTOP-2FCLKHN1.bwrouter>
2022-12-08 10:47:14 -0600

    Primer COMMIT

--- src/unidad3/examen.java
+++ src/unidad3/examen.java
@@ -9,9 +9,8 @@ package unidad3;
  * @author yovanny
  */
 public class examen {
-    int[] salarios = {2550,7890,6605,10900,8925,6350,7524};
-    for(String salario: salarios){
-            System.out.println(salarios)
-                    ;
-}
+    public static void main(String[] args) {
+        int[] salarios = {2550,7890,6605,10900,8925,6350,7524};
+        
+    }                 
 }
